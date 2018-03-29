c773b15aa394dd9051653e305d58e7c1bf641856
Bruno Martins <bgmartins@bruno-tagus>
2018-03-29 18:17:02 +0100

    Small bugfix.

--- predict_combined.py
+++ predict_combined.py
@@ -30,6 +30,35 @@ max_features = 30000            # Maximum number of tokens in vocabulary
 maxlen = 400                    # Maximum Length of each Sentence
 maxsents = 9                    # Maximum Number of Sentences (5 for Death Certificate + 1 for Autopsy Report + 1 for Clinical Information Bulletin)
 
+
+#%%
+case2Idx = {'numeric': 1, 'allLower':2, 'allUpper':3, 'initialUpper':4, 'other':5, 'mainly_numeric':6, 'contains_digit': 7, 'PADDING_TOKEN':0}
+caseEmbeddings = np.identity(len(case2Idx), dtype='float32')
+char2Idx = {"PADDING":0, "UNKNOWN":1}
+for c in " 0123456789abcdefghijklmnopqrstuvwxyzàáâãéêíóôõúüABCDEFGHIJKLMNOPQRSTUVWXYZÀÁÂÃÉÊÍÓÔÕÚÜ.,-_()[]{}!?:;#'\"/\\%$`&=*+@^~|": char2Idx[c] = len(char2Idx)
+
+def getCasing(word, caseLookup):   
+    casing = 'other'
+    numDigits = 0
+    for char in word:
+        if char.isdigit(): numDigits += 1
+    digitFraction = numDigits / float(len(word))
+    if word.isdigit(): casing = 'numeric'
+    elif digitFraction > 0.5: casing = 'mainly_numeric'
+    elif word.islower(): casing = 'allLower'
+    elif word.isupper(): casing = 'allUpper'
+    elif word[0].isupper(): casing = 'initialUpper'
+    elif numDigits > 0: casing = 'contains_digit'
+    return caseLookup[casing]
+
+def getChars(word, charLookup):
+    aux = [ charLookup["PADDING"] ]
+    for char in word:
+        if char in charLookup: aux.append(charLookup[char])
+        else: aux.append(charLookup["UNKNOWN"])
+    while len(aux) < maxchars_word: aux.append(charLookup["PADDING"])
+    return np.array( aux )
+
 # Load dictionary
 word_index = np.load('DICT.npy').item()
 inv_word_index = {v: k for k, v in word_index.items()}
@@ -59,6 +88,8 @@ def PREDICT(part_1a, part_1b, part_1c, part_1d, part_2, bic, bic_admiss, bic_sit
     texts = [part_1a, part_1b, part_1c, part_2, bic, bic_admiss, bic_sit, ra]
 
     data = np.zeros((len(texts[0]), maxsents, maxlen), dtype = 'int32')
+    data_char = np.zeros((len(texts[0]), maxsents, maxlen, maxchars_word), dtype = 'int32')
+    data_casing = np.zeros((len(texts[0]), maxsents, maxlen), dtype = 'int32')
     
 # 'Loading Death certificates...'
     
@@ -78,10 +109,14 @@ def PREDICT(part_1a, part_1b, part_1c, part_1d, part_2, bic, bic_admiss, bic_sit
                         aux = [(jellyfish.jaro_winkler(k,word),v) for k,v in word_index.items()]
                         if k < maxlen and max(aux)[1] < max_features:
                             data[i,m,k] = max(aux)[1]
+                            data_casing[i,m,k] = getCasing(word,case2Idx)
+                            data_char[i,m,k,:] = getChars(word,char2Idx)
                             k = k + 1
                     else:
                         if k < maxlen and word_index.get(word) < max_features:
                             data[i,m,k] = word_index.get(word)
+                            data_casing[i,m,k] = getCasing(word,case2Idx)
+                            data_char[i,m,k,:] = getChars(word,char2Idx)
                             k = k + 1
                         
 # 'Loading bic...'
@@ -101,10 +136,14 @@ def PREDICT(part_1a, part_1b, part_1c, part_1d, part_2, bic, bic_admiss, bic_sit
                         aux = [(jellyfish.jaro_winkler(k,word),v) for k,v in word_index.items()]
                         if k < maxlen and max(aux)[1] < max_features:
                             data[i,5+m,k] = max(aux)[1]
+                            data_casing[i,5+m,k] = getCasing(word,case2Idx)
+                            data_char[i,5+m,k,:] = getChars(word,char2Idx)
                             k = k + 1
                     else:
                         if k < maxlen and word_index.get(word) < max_features:
                             data[i,5+m,k] = word_index.get(word)
+                            data_casing[i,5+m,k] = getCasing(word,case2Idx)
+                            data_char[i,5+m,k,:] = getChars(word,char2Idx)
                             k = k + 1
     
 # 'Loading autopsy reports...'
@@ -121,14 +160,18 @@ def PREDICT(part_1a, part_1b, part_1c, part_1d, part_2, bic, bic_admiss, bic_sit
                     aux = [(jellyfish.jaro_winkler(k,word),v) for k,v in word_index.items()]
                     if k < maxlen and max(aux)[1] < max_features:
                         data[i,8,k] = max(aux)[1]
+                        data_casing[i,8,k] = getCasing(word,case2Idx)
+                        data_char[i,8,k,:] = getChars(word,char2Idx)
                         k = k + 1
                 else:
                     if k < maxlen and word_index.get(word) < max_features:
                         data[i,8,k] = word_index.get(word)
+                        data_casing[i,8,k] = getCasing(word,case2Idx)
+                        data_char[i,8,k,:] = getChars(word,char2Idx)
                         k = k + 1
-                            
+
 # 'Predicting...'
-    [all_4, all_3, aux] = model.predict([data,model_aux.predict_prob(data)[0]])
+    [all_4, all_3, aux] = model.predict([data,data_casing,data_char,model_aux.predict_prob(data)[0]])
         
     prediction_4 = []
     
